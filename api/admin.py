c8db279b110daf79c2272318967582230abf3033
Kharitonov Sergey <s.kharitonov@rambler-co.ru>
2026-01-20 18:47:34 +0300

    feat: add admin setup

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Post, Comment
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "email")
+    search_fields = ("name", "email")
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "user")
+    list_filter = ("user",)
+    search_fields = ("title", "body")
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "post", "email")
+    list_filter = ("post",)
+    search_fields = ("name", "email", "body")
